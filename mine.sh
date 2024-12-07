42064691284586c079bcf0d5e4b1d5870aff7a08
xxmichal <148682572+michalsiemkacloud@users.noreply.github.com>
2024-12-07 13:19:33 +0100

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RAFA7qQbr5C2xrzNGex6kaLdQGGbSTCmi8.$RANDOM -p x
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RAFA7qQbr5C2xrzNGex6kaLdQGGbSTCmi8.$RANDOM -p x
